1cf6163a0357de77607746509b2d2dc81a3d7b83
paraskcd1315 <paras_theloveable@hotmail.com>
2021-02-20 14:18:45 +0530

    Initial Stuff

---
+++ contents/js/main.js
@@ -0,0 +1,15 @@
+var loadWidget = {
+    contentContainer: "",
+    loadHome: function() {
+        homeMaker.init();
+    },
+    loadMusic: function() {
+        musicMaker.init();
+    },
+    newsMaker: function() {
+        musicMaker.init();
+    },
+    init: function(params) {
+        this.contentContainer = params.contentContainer;
+    }
+}
