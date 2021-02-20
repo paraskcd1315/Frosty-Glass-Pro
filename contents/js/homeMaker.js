904e8aa20e9769b5426fa6a8657368ecf39fa8f9
paraskcd1315 <paras_theloveable@hotmail.com>
2021-02-20 16:18:42 +0530

    Time Library made

--- contents/js/homeMaker.js
+++ contents/js/homeMaker.js
@@ -1,5 +1,16 @@
 var homeMaker = {
+    makeTimeContainer: function() {
+        let mainDiv = domMaker.init({
+                type: "div",
+                id: "timeContainer",
+            }),
+            greeter = domMaker.init({
+                type: "div",
+                id: "greeterContainer",
+            }),
+            
+    },
     init: function() {
-        
+        let timeContainer = this.makeTimeContainer();
     }
 }
