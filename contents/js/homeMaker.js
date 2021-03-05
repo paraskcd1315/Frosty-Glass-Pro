1b5778f42c9ba2195748af16920fd029168805c0
paraskcd1315 <paras_theloveable@hotmail.com>
2021-03-05 09:01:39 +0530

    Searchbar can now open urls too

--- contents/js/homeMaker.js
+++ contents/js/homeMaker.js
@@ -79,7 +79,16 @@ var homeMaker = {
         searchInput.addEventListener("blur", this.resetMoveUp, false);
         searchInput.addEventListener("keyup", (e) => {
             if(e.keyCode === 13) {
-                window.location = `https://www.google.com/search?q=${e.target.value}`;
+                let url = e.target.value;
+                let pattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+                if(e.target.value.substring(0, 8) === "https://" || e.target.value.substring(0,7) === "http://") {
+                    window.location = e.target.value;
+                } else if(pattern.test(url)) {
+                    let url = `https://${e.target.value}`;
+                    window.location = url;
+                } else {
+                    window.location = `https://www.google.com/search?q=${e.target.value}`;
+                }
                 e.target.value = "";
             }
         }, false)
