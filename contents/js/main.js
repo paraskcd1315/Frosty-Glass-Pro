22100afb0eeccae5d7cf5f1d6557bf79539b8f57
paraskcd1315 <paras_theloveable@hotmail.com>
2021-03-06 18:36:21 +0530

    fixed tons of bugs

--- contents/js/main.js
+++ contents/js/main.js
@@ -4,41 +4,49 @@ var lpmMode = false;
 var loadWidget = {
     contentContainer: "",
     populateStatusbarContainer: function(div, newData) {
-        const signal = domMaker.init({
-                type: "img",
-                id: "statusbarSignal",
-                src: "contents/icons/statusIcons/telephony/" + newData.telephony.bars + ".png"
-            }),
-            wifi = domMaker.init({
+        let signal;
+        let wifi;
+        if(document.getElementById("statusbarSignal")) {
+            div.removeChild(document.getElementById("statusbarSignal"));
+        }
+        if(document.getElementById("statusbarWifi")) {
+            div.removeChild(document.getElementById("statusbarWifi"));
+        }
+        if(newData.wifi.enabled) {
+           wifi = domMaker.init({
                 type: "img",
                 id: "statusbarWifi",
                 src: "contents/icons/statusIcons/wifi/" + newData.wifi.bars + ".png"
             });
-        if(document.getElementById("statusbarSignal")) {
-            div.removeChild(document.getElementById("statusbarSignal"));
-            div.removeChild(document.getElementById("statusbarWifi"));
+            div.prepend(wifi);
         }
-        div.prepend(wifi);
-        div.prepend(signal);
+        if(newData.telephony.type !== "") {
+            signal = domMaker.init({
+                type: "img",
+                id: "statusbarSignal",
+                src: "contents/icons/statusIcons/telephony/" + newData.telephony.bars + ".png"
+            });
+            div.prepend(signal);
+        }
     },
     populateWithBattery: function(div, newData) {
         let battlevel = Math.ceil((newData.battery.percentage) / 10) * 10;
         let battery;
         let batteryChargingURL = lpmMode ? "contents/icons/statusIcons/battery-charging-lpm/" + battlevel + ".png" : "contents/icons/statusIcons/battery-charging/" + battlevel + ".png";
-        let batteryURL = lpmMode ? "contents/icons/statusIcons/batter-lpm/" + battlevel + ".png" : "contents/icons/statusIcons/battery/" + battlevel + ".png";
-        switch(newData.state) {
+        let batteryURL = lpmMode ? "contents/icons/statusIcons/battery-lpm/" + battlevel + ".png" : "contents/icons/statusIcons/battery/" + battlevel + ".png";
+        switch(newData.battery.state) {
             case 0: 
                 battery = domMaker.init({
                     type: "img",
                     id: "statusbarBattery",
-                    src: batteryChargingURL
+                    src: batteryURL
                 });
                 break;
             case 1:
                 battery = domMaker.init({
                     type: "img",
                     id: "statusbarBattery",
-                    src: batteryURL
+                    src: batteryChargingURL
                 });
                 break;
             default:
@@ -47,6 +55,7 @@ var loadWidget = {
                     id: "statusbarBattery",
                     src: batteryURL
                 });
+                break;
         }
         if(document.getElementById("statusbarBattery")) {
             div.removeChild(document.getElementById("statusbarBattery"));
@@ -93,35 +102,37 @@ var loadWidget = {
             });
         });
         params.menuContainer.addEventListener("touchend", (e) => {
-            loadWidget.contentContainer.classList.add("hide");
-            for(let i = 0; i < params.menuContainer.children.length; i++) {
-                let child = params.menuContainer.children[i];
-                if(child.classList.contains("active")) {
-                    child.classList.remove("active");
+            if(e.target.id) {
+                loadWidget.contentContainer.classList.add("hide");
+                for(let i = 0; i < params.menuContainer.children.length; i++) {
+                    let child = params.menuContainer.children[i];
+                    if(child.classList.contains("active")) {
+                        child.classList.remove("active");
+                    }
                 }
+                setTimeout(() => {
+                    loadWidget.contentContainer.innerHTML = "";
+                }, 300);
+                switch(e.target.id) {
+                    case "homeMenu": 
+                        e.target.classList.add("active");
+                        setTimeout(() => {
+                            homeActive = true;
+                            loadWidget.loadHome();
+                        }, 360);
+                        break;
+                    case "musicMenu":
+                        e.target.classList.add("active");
+                        break;
+                    case "newsMenu":
+                        e.target.classList.add("active");
+                        break;
+                }
+                setTimeout(() => {
+                    loadWidget.contentContainer.classList.remove("hide");
+                }, 350);
             }
-            setTimeout(() => {
-                loadWidget.contentContainer.innerHTML = "";
-            }, 300);
-            switch(e.target.id) {
-                case "homeMenu": 
-                    e.target.classList.add("active");
-                    setTimeout(() => {
-                        homeActive = true;
-                        loadWidget.loadHome();
-                    }, 360);
-                    break;
-                case "musicMenu":
-                    e.target.classList.add("active");
-                    break;
-                case "newsMenu":
-                    e.target.classList.add("active");
-                    break;
-            }
-            setTimeout(() => {
-                loadWidget.contentContainer.classList.remove("hide");
-            }, 350);
-        })
+        });
         this.loadHome();
     }
 }
